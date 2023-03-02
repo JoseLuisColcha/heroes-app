92ae78f32a37a94c469c8e4fe1ed924683b3bc7f
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-03-01 22:17:40 -0500

    Add routers, LoginPage, SearchPage, MarvelPage and NavBar

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,5 +1,5 @@
-import React from "react";
+import { AppRouter } from './components/routers/AppRouter'
 
 export const HeroesApp = () => {
-  return <div>HeroesApp</div>;
-};
+	return <AppRouter />
+}
