d84137b67bb0e131e527a9457a643fa9647dcd14
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-03-02 11:30:35 -0500

    Create dashboard'routes

--- src/components/routers/AppRouter.js
+++ src/components/routers/AppRouter.js
@@ -1,21 +1,12 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import { DcPage } from '../dc/DcPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from '../login/LoginPage'
-import { MarvelPage } from '../marvel/MarvelPage'
-import SearchPage from '../search/SearchPage'
-import { Navbar } from '../ui/NavBar'
-
+import { DashboardRoutes } from './DashboardRoutes'
 export const AppRouter = () => {
 	return (
 		<BrowserRouter>
-			<Navbar />
-
 			<Routes>
-				<Route path='/' element={<MarvelPage />} />
-				<Route path='/marvel' element={<MarvelPage />} />
-				<Route path='/dc' element={<DcPage />} />
-				<Route path='/search' element={<SearchPage />} />
 				<Route path='/login' element={<LoginPage />} />
+				<Route path='/*' element={<DashboardRoutes />} />
 			</Routes>
 		</BrowserRouter>
 	)
