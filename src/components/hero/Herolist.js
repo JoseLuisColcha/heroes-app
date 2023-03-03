e8e6612c13e3acef35c4cb9707c534a1aa5835ed
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-03-02 21:11:54 -0500

    Optimise HeroList and HeroCard component

--- src/components/hero/Herolist.js
+++ src/components/hero/Herolist.js
@@ -1,8 +1,9 @@
+import { useMemo } from 'react'
 import { getHeroesByPublishers } from '../selectors/getHeroesByPublishers'
 import { HeroCard } from './HeroCard'
 
 export const Herolist = ({ publisher }) => {
-	const heroes = getHeroesByPublishers(publisher)
+	const heroes = useMemo(() => getHeroesByPublishers(publisher), [publisher])
 
 	return (
 		<div className='row rows-cols-1 row-cols-md-3'>
